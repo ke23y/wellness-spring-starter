85f6d317e0a7a5f818cd74958fd21c6b707aad33
Dave Clay <daveclay@gmail.com>
2022-04-08 12:56:26 -0700

    Add blank test

---
+++ src/test/java/com/walmart/interview/wellness/wellnessspringstarter/controller/SampleControllerTest.java
@@ -0,0 +1,12 @@
+package com.walmart.interview.wellness.wellnessspringstarter.controller;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleControllerTest {
+
+    @Test
+    void demoTestMethod() {
+        assertTrue(true);
+    }
+}
